25c6ac8f963249b4fa4b20ce96fe10d333c7a5bc
Jake Gamelin <jgam_compose@yahoo.com>
2021-02-26 04:57:50 -0800

    Added major sounds.

---
+++ Assets/PlayerMovementAudio.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementAudio : MonoBehaviour
+{
+    public GameObject mainCamera;
+    public void PlayAudio()
+    {
+        if(mainCamera)
+        {
+            AkSoundEngine.PostEvent("Play_Footsteps", mainCamera);
+        }
+    }
+}

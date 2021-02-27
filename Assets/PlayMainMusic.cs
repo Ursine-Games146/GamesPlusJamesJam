c260610142a57b5a351df4e72f0223817f5e878a
Jake Gamelin <jgam_compose@yahoo.com>
2021-02-26 16:36:10 -0800

    Fixed an issue of repeating music if game restarted.

---
+++ Assets/PlayMainMusic.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayMainMusic : MonoBehaviour
+{
+    static bool isPlaying;
+    void Start()
+    {
+        if(!isPlaying)
+        {
+            AkSoundEngine.PostEvent("Play_MainMusic", gameObject);
+            isPlaying = true;
+        }
+    }
+}
